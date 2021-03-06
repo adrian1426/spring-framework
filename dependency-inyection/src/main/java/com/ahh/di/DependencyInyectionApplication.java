8bd4c425414bcd08d92cf86fdce338c3e56daa7a
Adrian1426 <adrianhh1426@gmail.com>
2021-03-06 16:15:30 -0600

    beans, declaración de forma explicita

--- dependency-inyection/src/main/java/com/ahh/di/DependencyInyectionApplication.java
+++ dependency-inyection/src/main/java/com/ahh/di/DependencyInyectionApplication.java
@@ -5,27 +5,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import com.ahh.di.profiles.EnvironmentService;
-import com.ahh.di.qualifiers.Animal;
-import com.ahh.di.qualifiers.Avion;
-import com.ahh.di.qualifiers.Nido;
-import com.ahh.di.qualifiers.Pajaro;
-import com.ahh.di.scopes.EjemploScope;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DependencyInyectionApplication {
 	
 	private static final Logger log = LoggerFactory.getLogger(DependencyInyectionApplication.class);
+	
+	@Bean
+	public String getApplicationName() {
+		return "Adrian Dev";
+	}
 
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context=SpringApplication.run(DependencyInyectionApplication.class, args);
-		EjemploScope ejemploScope=context.getBean(EjemploScope.class);
-		EjemploScope ejemploScope2=context.getBean(EjemploScope.class);
+		String nombreApplication=context.getBean(String.class);
 		
-		log.info("Beans equals {}",ejemploScope.equals(ejemploScope2));
-		log.info("Beans == {}",ejemploScope==ejemploScope2);
+		log.info("Nombre aplicación: {}",nombreApplication);
 	}
 
 }
--- dependency-inyection/src/main/java/com/ahh/di/scopes/EjemploScopeService.java
+++ dependency-inyection/src/main/java/com/ahh/di/scopes/EjemploScopeService.java
@@ -13,6 +13,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Scope("prototype")
-public class EjemploScope {
+public class EjemploScopeService {
 
 }

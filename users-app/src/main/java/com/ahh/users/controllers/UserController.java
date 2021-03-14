6e42c696b0e07b8db1b95c51085b700cba40d538
Adrian1426 <adrianhh1426@gmail.com>
2021-03-13 18:40:08 -0600

    Endpoint obtener usuarios con ResponseEntity

--- users-app/src/main/java/com/ahh/users/controllers/UserController.java
+++ users-app/src/main/java/com/ahh/users/controllers/UserController.java
@@ -3,9 +3,17 @@
  */
 package com.ahh.users.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ahh.users.models.User;
+import com.ahh.users.services.UserService;
 
 /**
  * @author adria
@@ -13,11 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/users")
 public class UserController {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public String saludo() {
-		return "Hola mi amigo";
+	public ResponseEntity<List<User>> getUsers(){
+		return new ResponseEntity<List<User>>(userService.getUsers(),HttpStatus.OK);
 	}
 }
--- users-app/src/main/java/com/ahh/users/services/UserService.java
+++ users-app/src/main/java/com/ahh/users/services/UserService.java
@@ -3,9 +3,16 @@
  */
 package com.ahh.users.services;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ahh.users.models.User;
 import com.github.javafaker.Faker;
 
 /**
@@ -15,8 +22,20 @@ import com.github.javafaker.Faker;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private Faker faker;
 
+	private List<User> users = new ArrayList<>();
+
+	@PostConstruct
+	public void init() {
+		for (int i = 0; i < 100; i++) {
+			users.add(new User(faker.funnyName().name(), faker.name().username(), faker.dragonBall().character()));
+		}
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
 }
